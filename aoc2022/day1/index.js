bae62301f66f54909185eabc97c448f940b6bd9b
Abhay <80220229+EpicGamer007@users.noreply.github.com>
2022-12-03 05:07:29 +0000

    day 1

---
+++ aoc2022/day1/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+fs.readFile("./input", "utf8", function (err, data) {
+	if(err) {
+		return console.log(err);
+	}
+	const reindeer = data.split("\n\n");
+
+	const totals = [];
+	
+	reindeer.forEach((str) => {
+		const nums = str.split("\n").map(x => Number(x));
+		const totalLocal = nums.reduce((a, b) => { return +a + +b; });
+		totals.push(totalLocal);		
+	});
+
+	totals.sort((a, b) => a - b);
+	console.log("Part 1: " + totals[totals.length - 1]);
+	console.log("Part 2: " + (totals.pop()+totals.pop()+totals.pop()));
+	
+});
