432973304ed1e4c544b52b6a40de76cf55a41869
Abhay <80220229+EpicGamer007@users.noreply.github.com>
2022-12-06 05:41:58 +0000

    Improve bash file

--- main.sh
+++ main.sh
@@ -1,5 +1,18 @@
 #!/bin/env bash
 
-read -p "Enter day to run: " day
+read -p "Enter day to run (or a̲l̲l̲): " day
 clear
-node ./aoc2022/day$day
+
+if [ $day == "all" ]; then
+	folders=$(find aoc2022/*/*.js)
+	declare -i day=1
+	for folder in $folders
+	do
+		echo "Day $day:"
+		node $folder
+		echo ""
+		day=day+1
+	done
+else
+	node ./aoc2022/day$day
+fi
